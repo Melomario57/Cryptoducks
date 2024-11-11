ba57f129734ae71f27c3f488ccff399493fac959
Melomario57 <melomario57@gmail.com>
2024-11-11 15:37:18 -0600

    Se termina proyecto de práctica

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,24 +1,21 @@
+import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
-export default function ProtectedRoute({
-  isLoggedIn,
-  children,
-  anonymous = false,
-}) {
+import AppContext from "../contexts/AppContext";
+
+export default function ProtectedRoute({ children, anonymous = false }) {
   const location = useLocation();
   const from = location.state?.from || "/";
-  // Si el usuario ha iniciado la sesión le redirigimos fuera de nuestras
-  // rutas anónimas.
+
+  const { isLoggedIn } = useContext(AppContext);
+
   if (anonymous && isLoggedIn) {
     return <Navigate to={from} />;
   }
 
-  // Si el usuario no ha iniciado la sesión e intenta acceder a una ruta que
-  // requiere autorización, le redirigimos a la ruta /login.
   if (!anonymous && !isLoggedIn) {
     return <Navigate to="/login" state={{ from: location }} />;
   }
 
-  // En caso contrario, muestra los hijos de la ruta actual.
   return children;
 }
